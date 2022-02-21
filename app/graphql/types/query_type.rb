7a044f0b455a9c6e0350ba60ded93499394a81e2
Yunus Bulut <bulutt1404@gmail.com>
2022-02-21 13:45:29 +0300

    Destroy store and reservation to query type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,31 +1,17 @@
 module Types
   class QueryType < Types::BaseObject
+    add_field(GraphQL::Types::Relay::NodeField)
+
     field :stores, [Types::StoreType], null: false, camelize: false
 
     def stores
       Store.all
     end
 
-    field :store, Types::StoreType, null: false, camelize: false do
-      argument :id, ID, required: true
-    end
-
-    def store(id:)
-      Store.find(id)
-    end
-
     field :reservations, [Types::ReservationType], null: false, camelize: false
 
     def reservations
       Reservation.all
-    end
-
-    field :reservation, Types::ReservationType, null: false, camelize: false do
-      argument :id, ID, required: true
-    end
-
-    def reservation(id:)
-      Reservation.find(id)
     end
   end
 end
